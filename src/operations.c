2dde52a9ea9a84e448031f84b6b555d80e1c132c
skrmnghrdr <notpaulogrian@protonmail.com>
2025-08-18 22:57:26 -0400

    update operations

--- src/operations.c
+++ src/operations.c
@@ -31,15 +31,32 @@ int process_equation(struct unsolved_equation_t *unsolved_equ, struct solved_equ
      *
      */
      //test change to see if anything changes on the github
-    int return_value = -1;
+     int return_value = -1;
+     //make copies and convert to little endian
+     //! structs come with value.
+     //! it's already little endian, so we put it to normal to match the equ_viewer.py
+    uint32_t equation_id = htobe32(unsolved_equ->equation_id);
+    uint64_t first_operand = htobe64(unsolved_equ->ptr_equation.operand_first);
+    uint64_t second_operand = htobe64(unsolved_equ->ptr_equation.operand_second);
+    //! one byte deos not need to be endianized
+    uint8_t operator = unsolved_equ->ptr_equation.operator;
+    
     //! start processing things here
-    //! structs come with value.
     //! just deal with endianess and solve
     //! then plug to solved buffer
     //! and make another function to write it on the file
     //! with permissions
-    //well what if we just saved this but we did not commit, what would happen
-    //! load point anchor over here
+    
+    printf("The adjusted endiannes of the things are: \n");
+    printf("EquationID:0x%X 1st: 0x%lX OP:0x%X 2nd: 0x%lX\n",equation_id, first_operand, operator, second_operand);
+    //! for now, we just get the data and plug it back to write :)
+
+    solved_buffer->equation_id = equation_id;
+    solved_buffer->flags = 0x69; //99 temporarry
+    solved_buffer->type = 0x69;
+    solved_buffer->solution = 0x699888899999999;
+
+    //write this shit to a file somwhere and see
     return_value = 0;
     return return_value;
     /*\
