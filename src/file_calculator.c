3773c334927ae473a1fbeb681c6eaadf1d84b657
skrmnghrdr <notpaulogrian@protonmail.com>
2025-08-20 11:37:21 -0400

    fix CMakeLists.txt

--- src/file_calculator.c
+++ src/file_calculator.c
@@ -5,6 +5,7 @@
 
 int file_calculator(char * input_directory, char * output_directory)
 {
+   //**cleanse thy output here brother **/
    //! we might just call solve directory directly lol 
    solve_directory(input_directory, output_directory);
    //sanity check for operations include correctly
